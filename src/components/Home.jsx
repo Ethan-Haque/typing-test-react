52c8708469a9eb54aed76a6e92b17d2782dadfce
Ethan Haque <eth.haque@gmail.com>
2022-08-27 22:36:47 -0700

    Add fetch to home component and update styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+  const [typingText, setTypingText] = useState("begin");
+
+  function handleClick() {
+    // Text for typing test
+    fetch("http://metaphorpsum.com/paragraphs/1/9")
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (data) {
+        setTypingText(data);
+      });
+  }
+
   return (
     <div name="home" className="w-full h-screen bg-[#0d47a1]">
       {/* Container */}
       <div className="mx-auto px-8 flex flex-col items-center justify-center h-full">
         <div>
-          <button className=" text-white border-2 px-6 py-3 my-2  hover:bg-black">
-            Begin
-          </button>
+          <h2 className="text-xl  py-1 sm:text-2xl foxt-bold text-[#ccd6f6]">
+            Type
+          </h2>
+          <h1 className="text-xl sm:text-3xl foxt-bold text-[#ffffff]">
+            {typingText}
+          </h1>
+          <button onClick={handleClick}>Show text</button>
         </div>
       </div>
     </div>
