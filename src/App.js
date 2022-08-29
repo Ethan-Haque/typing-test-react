b1158e4667db000537f3f49abf71262f317fecd3
Ethan Haque <eth.haque@gmail.com>
2022-08-28 20:49:33 -0700

    Add typing test component, key press hook, and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from "./components/Home";
+import TypingTest from "./components/TypingTest";
 
 function App() {
   return (
     <div>
-      <Home/>
+      {/* <Home/> */}
+      <TypingTest/>
     </div>
   );
 }
